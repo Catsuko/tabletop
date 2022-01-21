c213012d35ab893163d27f2c91aea08a02f1b1d5
Lewis Reid <lreidwilliams@gmail.com>
2022-01-21 22:02:41 +1000

    incremented patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tabletop.MixProject do
   def project do
     [
       app: :tabletop,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
